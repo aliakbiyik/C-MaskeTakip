ef25ec7a3a33ea0ea03e60959a7922adb60251e2
Ali AKBIYIK <aliiakbiyikk@gmail.com>
2024-02-27 04:03:00 +0300

    Katmanlar hazırlandı

--- MaskeTakip/Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
+++ MaskeTakip/Business/obj/Debug/net8.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1edfdce481ea51ab687a35d70a893e19dff6e0eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MaskeTakip/Workaround/obj/Debug/net8.0/Workaround.AssemblyInfo.cs
+++ MaskeTakip/Workaround/obj/Debug/net8.0/Workaround.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Workaround")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1edfdce481ea51ab687a35d70a893e19dff6e0eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Workaround")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Workaround")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
